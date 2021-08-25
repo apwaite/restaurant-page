57f65b897179f0eebee7dca9f56b46029b3ab523
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-08-25 23:22:17 +0100

    Added placeholder text

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,7 @@
 const renderFooter = () => {
   const footer = document.createElement("footer");
   footer.classList.add("footer");
+  footer.textContent = "This is a footer";
   return footer;
 };
 
