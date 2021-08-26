a7a7a44285a16320b604c92fddeaa0c7ced52c17
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-08-26 22:15:27 +0100

    Added logo to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,14 @@
+import Logo from "../assets/bs_logo.svg";
+
 const renderHeader = () => {
   const header = document.createElement("header");
   // header.classList.add("header");
-  const h1 = document.createElement("h1");
-  h1.textContent = "Restaurant Page";
-  header.appendChild(h1);
+  const img = document.createElement("img");
+  img.src = Logo;
+  img.alt = "Burger Shot Logo";
+  // const h1 = document.createElement("h1");
+  // h1.textContent = "Restaurant Page";
+  header.appendChild(img);
   return header;
 };
 
