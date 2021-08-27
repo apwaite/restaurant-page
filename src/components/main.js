b584f36f8786f860915e6ba0ba1d9b5b33b1393f
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-08-27 20:36:03 +0100

    Removed text from element and changed class from main to main-home as home is already in use and was causing conflicts with styling

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,18 @@
 const renderHome = () => {
   const home = document.createElement("main");
-  home.classList.add("home");
-  home.textContent = "Hi";
+  home.classList.add("main-home");
+  const colLeft = document.createElement("section");
+  colLeft.classList.add("col-left");
+  home.appendChild(colLeft);
+  const statement = document.createElement("p");
+  statement.classList.add("statement");
+  statement.textContent =
+    '"At Burger Place we value each and every one of our customers. That\'s why we go above and beyond to craft our traditional gourmet burgers, fries and shakes to create the perfect infusion of flavours that will leave you wanting more."';
+  colLeft.appendChild(statement);
+  const colRight = document.createElement("section");
+  colRight.classList.add("col-right");
+  home.appendChild(colRight);
+
   return home;
 };
 
