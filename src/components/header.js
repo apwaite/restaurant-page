bc2a9718ae03139f64933674821b6ccfbaf19cf7
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-08-27 20:33:56 +0100

    Removed commented out code that was no longer in use

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,9 @@
-import Logo from "../assets/bs_logo.svg";
-
 const renderHeader = () => {
   const header = document.createElement("header");
   // header.classList.add("header");
-  const img = document.createElement("img");
-  img.src = Logo;
-  img.alt = "Burger Shot Logo";
-  // const h1 = document.createElement("h1");
-  // h1.textContent = "Restaurant Page";
-  header.appendChild(img);
+  const h1 = document.createElement("h1");
+  h1.textContent = "Restaurant Page";
+  header.appendChild(h1);
   return header;
 };
 
