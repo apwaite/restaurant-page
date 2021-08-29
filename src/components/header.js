f01a18fa9b3429a13ec885cfe90199a78fa8efb3
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-08-30 00:04:55 +0100

    Changed h1ttext to something more relevant

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ const renderHeader = () => {
   const header = document.createElement("header");
   // header.classList.add("header");
   const h1 = document.createElement("h1");
-  h1.textContent = "Restaurant Page";
+  h1.textContent = "Burger Place";
   header.appendChild(h1);
   return header;
 };
