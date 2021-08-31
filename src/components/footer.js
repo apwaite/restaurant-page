73bdd46c1214e12746d562184fb7e3efa6d7632e
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-08-31 23:51:21 +0100

    Renamed variable from p1 to p as only one p element is required

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,14 @@
 const renderFooter = () => {
   const footer = document.createElement("footer");
   footer.classList.add("footer");
-  const p1 = document.createElement("p");
-  p1.classList.add("made-by");
-  p1.textContent = "Made with ❤️ by Andrew";
+  const p = document.createElement("p");
+  p.classList.add("made-by");
+  p.textContent = "Made with ❤️ by Andrew";
   const link = document.createElement("a");
   link.href = "https://github.com/apwaite";
   link.target = "_blank";
-  p1.appendChild(link);
-  footer.appendChild(p1);
+  p.appendChild(link);
+  footer.appendChild(p);
   return footer;
 };
 
