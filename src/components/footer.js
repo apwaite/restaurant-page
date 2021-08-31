ffdede99660e3dea8dfd35742c4bd04a5056bd08
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-08-31 23:50:00 +0100

    Added link to GitHub profile

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,10 @@ const renderFooter = () => {
   const p1 = document.createElement("p");
   p1.classList.add("made-by");
   p1.textContent = "Made with ❤️ by Andrew";
+  const link = document.createElement("a");
+  link.href = "https://github.com/apwaite";
+  link.target = "_blank";
+  p1.appendChild(link);
   footer.appendChild(p1);
   return footer;
 };
