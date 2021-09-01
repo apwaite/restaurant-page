888b8465f78e710d48b3883f7f6c2f49c0c2bb34
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-09-01 23:57:17 +0100

    Commented out logic that has been moved to components/initialRender.js. Keeping hold of these just now in case I need to revert back

--- src/index.js
+++ src/index.js
@@ -1,43 +1,45 @@
 import "normalize.css";
-import { renderHeader } from "./components/header";
-import { renderNav } from "./components/nav";
-import { renderAbout } from "./components/about";
-import { renderMenu } from "./components/menu";
-import { renderContact } from "./components/contact";
-import { renderFooter } from "./components/footer";
+// import { renderHeader } from "./components/header";
+// import { renderNav } from "./components/nav";
+// import { renderAbout } from "./components/about";
+// import { renderMenu } from "./components/menu";
+// import { renderContact } from "./components/contact";
+// import { renderFooter } from "./components/footer";
+import initialRender from "./components/initialRender";
 
 console.log("Connected!");
 
 renderPage();
 
-function createNavEvents() {
-  const main = document.querySelector(".main");
-  const about = document.querySelector(".about");
-  const menu = document.querySelector(".menu");
-  const contact = document.querySelector(".contact");
+// function createNavEvents() {
+//   const main = document.querySelector(".main");
+//   const about = document.querySelector(".about");
+//   const menu = document.querySelector(".menu");
+//   const contact = document.querySelector(".contact");
 
-  about.addEventListener("click", () => {
-    main.removeChild(main.firstChild);
-    main.appendChild(renderAbout());
-  });
-  menu.addEventListener("click", () => {
-    main.removeChild(main.firstChild);
-    main.appendChild(renderMenu());
-  });
-  contact.addEventListener("click", () => {
-    main.removeChild(main.firstChild);
-    main.appendChild(renderContact());
-  });
-}
+//   about.addEventListener("click", () => {
+//     main.removeChild(main.firstChild);
+//     main.appendChild(renderAbout());
+//   });
+//   menu.addEventListener("click", () => {
+//     main.removeChild(main.firstChild);
+//     main.appendChild(renderMenu());
+//   });
+//   contact.addEventListener("click", () => {
+//     main.removeChild(main.firstChild);
+//     main.appendChild(renderContact());
+//   });
+// }
 
 function renderPage() {
-  const content = document.getElementById("content");
-  content.appendChild(renderHeader());
-  content.appendChild(renderNav());
-  const section = document.createElement("section");
-  section.classList.add("main");
-  content.appendChild(section);
-  section.appendChild(renderAbout());
-  content.appendChild(renderFooter());
-  createNavEvents();
+  initialRender();
+  // const content = document.getElementById("content");
+  // content.appendChild(renderHeader());
+  // content.appendChild(renderNav());
+  // const section = document.createElement("section");
+  // section.classList.add("main");
+  // content.appendChild(section);
+  // section.appendChild(renderAbout());
+  // content.appendChild(renderFooter());
+  // createNavEvents();
 }
