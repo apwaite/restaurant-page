2ac187d86b1916ddc7236f1ea371510f2f7874ab
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-09-02 00:00:48 +0100

    Attempting to add menu items to the menu page using an array of menu items. Currently having an issue appending this content - currently lookinh into this

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,6 +1,50 @@
+console.log("Loaded menu");
+
+function newMenuItem(src, alt, name, cost) {
+  const menuItem = document.createElement("div");
+  menuItem.classList.add("menu-item");
+  const img = document.createElement("img");
+  img.src = src;
+  img.alt = alt;
+  const itemInfo = document.createElement("div");
+  itemInfo.classList.add("info");
+  const itemName = document.createElement("h3");
+  itemName.textContent = name;
+  const itemCost = document.createElement("h2");
+  itemCost.textContent = cost;
+  itemInfo.appendChild(itemName);
+  itemInfo.appendChild(itemCost);
+  menuItem.appendChild(img);
+  menuItem.appendChild(itemInfo);
+}
+
 const renderMenu = () => {
   const menu = document.createElement("main");
   menu.classList.add("main-menu");
+  const menuItems = [
+    newMenuItem(
+      "../../dist/assets/bacon-cheese.jpg",
+      "Bacon Cheeseburger",
+      "Bacon Cheeseburger",
+      "$12.99"
+    ),
+    newMenuItem(
+      "../../dist/assets/bacon-double-cheese.jpg",
+      "Bacon Double Cheeseburger",
+      "Bacon Double Cheeseburger",
+      "$8.99"
+    ),
+    newMenuItem(
+      "../../dist/assets/ice-cold-soda.jpg",
+      "Ice Cold Soda",
+      "Ice Cold Soda",
+      "Small: $1.99, Medium: $2.99, Large: $3.99"
+    ),
+  ];
+
+  menuItems.map((item) => {
+    menu.appendChild(item);
+  });
   return menu;
 };
 
