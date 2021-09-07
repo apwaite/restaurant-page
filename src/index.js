d0cfcb5460788eeb10571be0aabe34af163868d0
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-09-07 23:03:19 +0100

    Cleaned up code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import "normalize.css";
-// import { renderHeader } from "./components/header";
-// import { renderNav } from "./components/nav";
-// import { renderAbout } from "./components/about";
-// import { renderMenu } from "./components/menu";
-// import { renderContact } from "./components/contact";
-// import { renderFooter } from "./components/footer";
 import initialRender from "./components/initialRender";
 
 console.log("Connected!");
