87e5567edf04ec96a5978e2dddf428abcb84ecb7
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-09-08 22:05:56 +0100

    Modified layout to make it uniform across screen sizes

--- src/components/about.js
+++ src/components/about.js
@@ -1,15 +1,12 @@
 const renderAbout = () => {
   const about = document.createElement("main");
   about.classList.add("main-about");
-  const con0 = document.createElement("section");
-  con0.classList.add("con-0");
-  about.appendChild(con0);
-  const con1 = document.createElement("section");
-  con1.classList.add("con-1");
-  about.appendChild(con1);
+  const mission = document.createElement("section");
+  mission.classList.add("mission");
+  about.appendChild(mission);
   const inner1 = document.createElement("div");
   inner1.classList.add("inner-1");
-  con1.appendChild(inner1);
+  mission.appendChild(inner1);
   const img = document.createElement("img");
   img.classList.add("chef");
   img.src = "../../dist/assets/chef.jpg";
@@ -17,12 +14,16 @@ const renderAbout = () => {
   inner1.appendChild(img);
   const inner2 = document.createElement("div");
   inner2.classList.add("inner-2");
-  con1.appendChild(inner2);
+  mission.appendChild(inner2);
   const statement = document.createElement("p");
   statement.classList.add("statement");
   statement.textContent =
     '"At Burger Place we value each and every one of our customers. That\'s why we go above and beyond when crafting our traditional gourmet burgers, fries and shakes in order to create the perfect infusion of flavours that will leave you wanting more."';
   inner2.appendChild(statement);
+  const con1 = document.createElement("section");
+  con1.classList.add("con-1");
+  about.appendChild(con1);
+
   return about;
 };
 
