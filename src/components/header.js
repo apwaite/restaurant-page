9cf5d93cc5c1fec3f5670ed95415448921bb9eb2
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-09-10 21:33:46 +0100

    Added Burger Place logo to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,11 @@
 const renderHeader = () => {
   const header = document.createElement("header");
   // header.classList.add("header");
+  const logo = document.createElement("img");
+  logo.classList.add("logo");
+  logo.src = "../../dist/assets/bp-logo.png";
+  logo.alt = "Burger Place Logo";
+  header.appendChild(logo);
   const h1 = document.createElement("h1");
   h1.textContent = "Burger Place";
   header.appendChild(h1);
