de3c902671c5f1dd1d9aa03b28939a4dbfd063c3
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-09-13 23:23:44 +0100

    Added a feature to select current nav link when active, also removes previous nav link

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,8 +1,17 @@
 console.log("Loaded contact");
 
+function activeNavLink() {
+  const contact = document.querySelector(".contact");
+  const currentActive = document.querySelector(".active");
+  console.log(currentActive);
+  if (currentActive) currentActive.classList.remove("active");
+  contact.classList.add("active");
+}
+
 const renderContact = () => {
   const contact = document.createElement("main");
   contact.classList.add("main-contact");
+  activeNavLink();
   return contact;
 };
 
