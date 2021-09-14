10d81a62f91938a6132013afdb568181a1114a54
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-09-14 21:03:04 +0100

    Cleaned up code

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,18 +4,15 @@ const renderNav = () => {
   links.classList.add("links");
   nav.appendChild(links);
   const about = document.createElement("li");
-  // about.setAttribute("id", "about");
   about.classList.add("about");
   about.textContent = "About";
   links.appendChild(about);
   const menu = document.createElement("li");
-  // menu.setAttribute("id", "menu");
   menu.classList.add("menu");
   menu.textContent = "Menu";
   links.appendChild(menu);
   const contact = document.createElement("li");
   contact.classList.add("contact");
-  // contact.setAttribute("id", "contact");
   contact.textContent = "Contact";
   links.appendChild(contact);
   return nav;
