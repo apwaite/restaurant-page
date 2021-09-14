396872f723c59b066fdcd90c8c8d747f2cdbeee3
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-09-14 06:24:20 +0100

    Added console logs to code to determine where fault is coming from

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,15 @@
+import { renderAbout } from "./about";
+
 console.log("Loaded contact");
 
 function activeNavLink() {
   const contact = document.querySelector(".contact");
   const currentActive = document.querySelector(".active");
+  console.log("Before remove:");
   console.log(currentActive);
   if (currentActive) currentActive.classList.remove("active");
+  console.log("After remove:");
+  console.log(currentActive);
   contact.classList.add("active");
 }
 
