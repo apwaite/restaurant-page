283127a7872f2cbfd71a5ff5c4d0d9cba2f7ae25
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-09-15 03:02:29 +0100

    Added a new menu item

--- src/components/menu.js
+++ src/components/menu.js
@@ -7,9 +7,14 @@ function activeNavLink() {
   menu.classList.add("active");
 }
 
-const newMenuItem = (src, alt, name, cost) => {
+const newMenuItem = (id, src, alt, name, cost) => {
   const menuItem = document.createElement("div");
   menuItem.classList.add("menu-item");
+  if (id % 2 === 0) {
+    menuItem.classList.add("light");
+  } else {
+    menuItem.classList.add("dark");
+  }
   const img = document.createElement("img");
   img.src = src;
   img.alt = alt;
@@ -32,34 +37,46 @@ const renderMenu = () => {
   activeNavLink();
   const menuItems = [
     newMenuItem(
+      0,
       "../../dist/assets/bacon-cheese.jpg",
       "Bacon Cheeseburger",
       "Bacon Cheeseburger",
-      "$12.99"
+      "$12.99, or make it a meal (soda + fries): $15.99"
     ),
     newMenuItem(
+      1,
       "../../dist/assets/bacon-double-cheese.jpg",
       "Bacon Double Cheeseburger",
       "Bacon Double Cheeseburger",
-      "$8.99"
+      "$8.99, or make it a meal (soda + fries): $11.99"
     ),
     newMenuItem(
+      2,
       "../../dist/assets/crispy-chicken.jpg",
       "Crispy Chicken Burger",
       "Crispy Chicken Burger",
-      "$10.99"
+      "$10.99, or make it a meal (soda + fries): $13.99"
     ),
     newMenuItem(
+      3,
       "../../dist/assets/ice-cold-soda.jpg",
       "Ice Cold Soda",
       "Ice Cold Soda",
       "Small: $1.99, Medium: $2.99, Large: $3.99"
     ),
     newMenuItem(
+      4,
       "../../dist/assets/milkshake.jpg",
       "Milkshake",
       "Milkshake",
-      "Small: $3.99, Medium: $5.99, Large: £7.99)"
+      "Small: $3.99, Medium: $5.99, Large: £7.99"
+    ),
+    newMenuItem(
+      5,
+      "../../dist/assets/french-fries.jpg",
+      "French Fries",
+      "French Fries",
+      "On their own: $4.99"
     ),
   ];
 
