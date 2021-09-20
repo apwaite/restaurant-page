7a6054016f6850089987ee85a71f6eaa29d1217e
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-09-21 00:16:41 +0100

    Added content to social media section of contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,33 +4,52 @@ console.log("Loaded contact");
 
 function activeNavLink() {
   const contact = document.querySelector(".contact");
+
   const currentActive = document.querySelector(".active");
+
   if (currentActive) currentActive.classList.remove("active");
+
   contact.classList.add("active");
 }
 
 const renderContact = () => {
   const contact = document.createElement("main");
   contact.classList.add("main-contact");
+
   activeNavLink();
+
   const details = document.createElement("section");
   details.classList.add("details");
   contact.appendChild(details);
+
   const openingTimes = document.createElement("h2");
   openingTimes.textContent = "Opening Times:";
   details.appendChild(openingTimes);
+
   const openingTimesWeek = document.createElement("h3");
   openingTimesWeek.textContent = "MON-FRI: 11:00 - 22:00";
   details.appendChild(openingTimesWeek);
+
   const openingTimesWeekend = document.createElement("h3");
   openingTimesWeekend.textContent = "SAT-SUN: 13:00 - 23:00";
   details.appendChild(openingTimesWeekend);
+
   const socialMedia = document.createElement("section");
   socialMedia.classList.add("social-media");
   contact.appendChild(socialMedia);
+
+  const socialMediaContainer = document.createElement("div");
+  socialMediaContainer.classList.add("sm-links");
+  socialMedia.appendChild(socialMediaContainer);
+
+  const socialMediaIcons = document.createElement("p");
+  socialMediaIcons.textContent = "TODO: Add social media icons";
+  socialMediaContainer.appendChild(socialMediaIcons);
+
   const location = document.createElement("section");
   location.classList.add("location");
   contact.appendChild(location);
+
   return contact;
 };
 
