a368343cf63d5b44078da2e5844e5efccc32be8d
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-09-29 00:55:33 +0100

    Added styling to location-image class

--- src/components/contact.js
+++ src/components/contact.js
@@ -43,7 +43,7 @@ const renderContact = () => {
   locationImage.classList.add("location-image");
   location.appendChild(locationImage);
   const image = document.createElement("img");
-  image.src = "";
+  image.src = "../../dist/assets/burger-place-location.jpg";
   image.alt = "Burger Place Location";
   locationImage.appendChild(image);
   const locationAddress = document.createElement("div");
