162f4be1be7b2009e6224dee6096a3a67bbb8acb
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-09-30 01:26:12 +0100

    Changed variable name to madeBy instead of p in preparation for a new footer element

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,17 @@
 const renderFooter = () => {
   const footer = document.createElement("footer");
   footer.classList.add("footer");
-  const p = document.createElement("p");
-  p.classList.add("made-by");
-  p.textContent = "Made with ❤️ by Andrew";
+  const madeBy = document.createElement("p");
+  madeBy.classList.add("made-by");
+  madeBy.textContent = "Made with ❤️ by Andrew";
   const link = document.createElement("a");
   link.href = "https://github.com/apwaite";
   link.target = "_blank";
-  p.appendChild(link);
+  madeBy.appendChild(link);
   const span = document.createElement("span");
   span.classList.add("fab", "fa-github");
   link.appendChild(span);
-  footer.appendChild(p);
+  footer.appendChild(madeBy);
   return footer;
 };
 
