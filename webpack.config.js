37993cbf2fe065068f31255c20955fdae9e73843
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-10-06 21:50:18 +0100

    Cleaned up code

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,9 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
